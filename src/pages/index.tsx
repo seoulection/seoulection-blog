df3907bc170ee649b2ff39673679364f57b74831
seoulection <6445410+seoulection@users.noreply.github.com>
2021-02-27 10:22:26 -0800

    cleans up more boilerplate

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import Layout from "../components/Layout";
 import BasicMeta from "../components/meta/BasicMeta";
 import OpenGraphMeta from "../components/meta/OpenGraphMeta";
-import TwitterCardMeta from "../components/meta/TwitterCardMeta";
 import { SocialList } from "../components/SocialList";
 
 export default function Index() {
@@ -9,7 +8,6 @@ export default function Index() {
     <Layout>
       <BasicMeta url={"/"} />
       <OpenGraphMeta url={"/"} />
-      <TwitterCardMeta url={"/"} />
       <div className="container">
         <div>
           <h1>
